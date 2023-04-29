347b085861929b3a57845434c96ab3615da86d07
Ryuk124 <60621197+Ryuk124@users.noreply.github.com>
2023-04-30 04:49:12 +0500

    Меню [Начало]
    Начал прикручивать основные функции в меню. Пока всё выглядит очень страшно, потому что я не стал искать прототипы и сразу начал пилить функции.
    
    Сделал кнопку входа и выхода из игры и переход в меню настроек. Завтра надеюсь допилить меню настроек и накинуть на него спрайтов. Хочу спать(

---
+++ Fiit-game-project/Assets/Scripts/MenuManager.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+
+    //не сработает в самом юнити, только если забилдить и запустить игру
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
