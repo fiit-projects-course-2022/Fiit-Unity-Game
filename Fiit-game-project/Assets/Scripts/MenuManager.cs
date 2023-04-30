74201f8030ab854b1e5b2ac1882984948a3fdc27
Ryuk124 <60621197+Ryuk124@users.noreply.github.com>
2023-04-30 16:37:17 +0500

    Меню [Завершение прототипа]
    Закончил создание основного функционала кнопок меню
    
    Теперь есть кнопки входа/выхода из игры, а также меню настроек. В меню настроек изменяется звук, графика, разрешение и есть checkbox с полным экраном
    
    Дальнейшую работу над меню продолжу когда мне скинут арты.
    либо придётся искать их самому(((

--- Fiit-game-project/Assets/Scripts/MenuManager.cs
+++ Fiit-game-project/Assets/Scripts/MenuManager.cs
@@ -1,19 +1,71 @@
-using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.Audio;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MenuManager : MonoBehaviour
 {
+    public AudioMixer audioMixer;
+    public TMP_Dropdown resolutionDropdown;
+    private Resolution[] resolutions;
+
+
+    void Start()
+    {
+        resolutions = Screen.resolutions;
+        resolutionDropdown.ClearOptions();
+        List<string> options = new List<string>();
+        
+        int currentResolutionIndex = 0;
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + "x" + resolutions[i].height;
+            options.Add(option);
+
+            if (resolutions[i].width == Screen.currentResolution.width &&
+                resolutions[i].height == Screen.currentResolution.height)
+                currentResolutionIndex = i;
+        }
+
+        resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = currentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
+    }
+
     public void PlayGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
-
 
     //не сработает в самом юнити, только если забилдить и запустить игру
     public void QuitGame()
     {
         Application.Quit();
     }
+
+    public void SetVolume(float volume)
+    {
+        audioMixer.SetFloat("volume", volume);
+    }
+
+    public void SetQuality(int qualityIndex)
+    {
+        QualitySettings.SetQualityLevel(qualityIndex);
+    }
+
+    //тоже нельзя увидеть в редакторе. Только в билде
+    public void SetFullScreen(bool fullScreen)
+    {
+        Screen.fullScreen = fullScreen;
+    }
+
+    public void SetResolution(int resolutionIndex)
+    {
+        Resolution resolution = resolutions[resolutionIndex];
+
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen); 
+    }
+
 }
