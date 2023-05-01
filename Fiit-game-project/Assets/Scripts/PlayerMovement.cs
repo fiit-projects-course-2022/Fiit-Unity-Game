0bbfc703ef49b56216abee28b5dd380a5db46cdd
Ryuk124 <60621197+Ryuk124@users.noreply.github.com>
2023-05-02 01:43:28 +0500

    Управление [Прототип, базовые функции]
    Написал 2 скрипта (PlayerMovement, CharacterController), благодаря которым персонаж теперь может перемещаться влево-вправо и прыгать
    
    Пока накатил лишь на болванки в виде стандартных объектов из юнити, но модифицировать под нормальные модельки несложно

---
+++ Fiit-game-project/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController2D controller;
+    public float runSpeed = 40;
+
+    private float horizontalMove;
+    private bool jump;
+
+    void Update()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+        if (Input.GetButtonDown("Jump"))
+            jump = true;
+    }
+
+
+    //меняешь коллайдеры - засунь в них любой нулевой physics
+    void FixedUpdate()
+    {
+        controller.Move(horizontalMove * Time.fixedDeltaTime, false, jump);
+        jump = false;
+    }
+}
