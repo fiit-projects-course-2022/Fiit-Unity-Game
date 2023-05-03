b90493352a009e9685eb05f64ba1c12fbc75903e
VirteN <61062830+semon24@users.noreply.github.com>
2023-05-03 19:46:02 +0500

    фон+фотки

---
+++ Fiit-game-project/Assets/Scripts/cameraMove.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraMove : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        hero - GameO
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
